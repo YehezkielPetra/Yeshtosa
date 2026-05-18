e5285bf8d95fb0666da8d8df334f4635062e41b7
Yehezkiel Petra <Yehezkielpetra.ypk@gmail.com>
2026-05-18 20:40:42 +0700

    fix: calibrate ssl authentication for cross region cluster

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
@@ -6,9 +6,12 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    // GANTI MENJADI STRING INI
+    ssl: "Amazon RDS" 
 });
 
-module.exports = pool.promise();
+module.exports = pool;
